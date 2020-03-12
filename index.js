e0ae4cba1c6a0abb8ef0a65e40d5736f530b1416
nicolasances <nicolas.matteazzi@gmail.com>
2020-03-12 15:24:39 +0100

    fix path

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var apiName = 'mongo-restore';
 
 var api = new Controller(apiName);
 
-api.path('POST', '/restores', (req, res) => {postMongoRestoreDlg})
+api.path('POST', '/restores', postMongoRestoreDlg)
 
 api.listen();
